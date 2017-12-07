209904d3f17203f0d43af1a5f7bcc804fee54f8f
ewilys <lisa.martini@insa-lyon.fr>
2017-12-07 16:34:50 +0100

    get date info and temperature info just need to set the json correctly

--- Yun/tempSensor/tempSensor.ino
+++ Yun/tempSensor/tempSensor.ino
@@ -2,15 +2,18 @@
 #include <DallasTemperature.h>
 #include <Bridge.h>
 #include <HttpClient.h>
-
+#include <Process.h>
 #include <Console.h>
 
 #define ONE_WIRE_BUS 2
 
 
-
+Process date;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+// Initialize the client library
+HttpClient client;
+int room;
 
 void setup(void) {
 
@@ -22,13 +25,13 @@ void setup(void) {
   SerialUSB.begin(9600);
   while (!SerialUSB); // wait for a serial connection
   SerialUSB.println("You're Connected");
+  room = 205;
   /*Console.begin(); //with network 
   while(!Console){
     ;
   }
   Console.println("You're Connected");*/
-  // Initialize the client library
-  HttpClient client;
+  
 }
 
 void loop(void) {
@@ -37,9 +40,33 @@ void loop(void) {
   // Make a HTTP request:
   //client.get("http://hoomieserver.herokuapp.com/temperature/year/2017");
 
-  sensors.requestTemperatures();
-  SerialUSB.println(sensors.getTempCByIndex(0));
+  
+  
+  SerialUSB.println(measureTemp());
+  /*while (client.available()) {
+    char c = client.read();
+    SerialUSB.print(c);
+    
+  }
+  SerialUSB.print("\n");*/
   SerialUSB.flush();
-  delay(10000); //wait every 10 sec
+  delay(3000); //wait every 10 sec 
 
 }
+
+float measureTemp(){
+  date.runShellCommand("echo `date +%Y-%m-%d-%H-%M-%S`");
+  // do nothing until the process finishes, so you get the whole output:
+  while (date.running());
+  //if there's a result from the date process, get it.
+  while (date.available()>0) {
+    // print the results we got.
+    SerialUSB.println(date.readString());
+  }
+  
+  sensors.requestTemperatures();
+  return sensors.getTempCByIndex(0);
+  
+}
+
+
