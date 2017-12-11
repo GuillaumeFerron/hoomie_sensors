ad059aa7fc91c6328d7564f77376fa07ed1fb808
ewilys <lisa.martini@insa-lyon.fr>
2017-12-11 17:57:05 +0100

    update code to good sending

--- Yun/tempSensor/tempSensor.ino
+++ Yun/tempSensor/tempSensor.ino
@@ -4,11 +4,15 @@
 #include <HttpClient.h>
 #include <Process.h>
 #include <Console.h>
+#include <stdio.h>
 
 #define ONE_WIRE_BUS 2
-
+#define ROOM_NUMBER 205
+#define NB_MEASURE_PER_SEND 4
+#define DELAY 30000
 
 Process date;
+Process sendData;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 // Initialize the client library
@@ -18,54 +22,82 @@ int room;
 void setup(void) {
 
   sensors.begin();
-
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   Bridge.begin();
 
   SerialUSB.begin(9600);
   while (!SerialUSB); // wait for a serial connection
   SerialUSB.println("You're Connected");
-  room = 205;
-  /*Console.begin(); //with network 
-  while(!Console){
-    ;
-  }
-  Console.println("You're Connected");*/
+  room = ROOM_NUMBER;
+ 
   
 }
 
 void loop(void) {
   
-
+  int nb_measure = 0;
+  String json="[";
   // Make a HTTP request:
-  //client.get("http://hoomieserver.herokuapp.com/temperature/year/2017");
-
-  
-  
-  SerialUSB.println(measureTemp());
-  /*while (client.available()) {
-    char c = client.read();
-    SerialUSB.print(c);
+  while( nb_measure < NB_MEASURE_PER_SEND ){
+    digitalWrite(LED_BUILTIN, HIGH);
+    String tempDoc = measureTemp();
+    SerialUSB.println(tempDoc);
+    json += tempDoc;
+    SerialUSB.flush();
+    digitalWrite(LED_BUILTIN, LOW);
+    if(nb_measure != NB_MEASURE_PER_SEND-1){
+      json += ",";
+      delay(DELAY); //wait every 30 sec 
+    }
     
+    nb_measure +=1;
   }
-  SerialUSB.print("\n");*/
-  SerialUSB.flush();
-  delay(3000); //wait every 10 sec 
+  json += "]";
+  
+  String json_to_send = "{\"data\":"+json+"}";
+  SerialUSB.println(json_to_send);
+  
+  if(!sendData.running()){
+    sendData.begin("curl");
+    sendData.addParameter("-X");
+    sendData.addParameter("POST");
+    sendData.addParameter("http://hoomieserver.herokuapp.com/temperature/addDoc");
+    sendData.addParameter("-H");
+    sendData.addParameter("Content-Type:application/json");
+    sendData.addParameter("--data-binary");
+    sendData.addParameter(json_to_send);
+    SerialUSB.println("sent");
+    sendData.run();
+  }
+  
 
 }
 
-float measureTemp(){
+String measureTemp(){
+  String tempInfo,dateVal;
+
   date.runShellCommand("echo `date +%Y-%m-%d-%H-%M-%S`");
   // do nothing until the process finishes, so you get the whole output:
   while (date.running());
   //if there's a result from the date process, get it.
-  while (date.available()>0) {
+  while (date.available()) {
     // print the results we got.
-    SerialUSB.println(date.readString());
+    char c = date.read();
+    if(c!='\n') dateVal += c;
   }
   
+  tempInfo = "{\"date\": \""+dateVal+"\",";
   sensors.requestTemperatures();
-  return sensors.getTempCByIndex(0);
+  char tempVal[5];
+  dtostrf(sensors.getTempCByIndex(0),5,2,tempVal);
+  String tempValString ;
+  for (int i=0;i<5;i++){
+    tempValString += tempVal[i];
+  }
+  tempInfo +=  "\"value\": "+tempValString+", \"room\": "+room+"}";
+  
+  return tempInfo;
   
 }
 
