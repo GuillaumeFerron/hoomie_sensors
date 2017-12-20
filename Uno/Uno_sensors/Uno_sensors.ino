47d911aa7057c7d40f983e29b1cb917be5774e5a
ewilys <lisa.martini@insa-lyon.fr>
2017-12-20 17:59:26 +0100

    add uno sensors code + update on yun code for lora msg, need to verif lora msg before sending to server

---
+++ Uno/Uno_sensors/Uno_sensors.ino
@@ -0,0 +1,211 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <TimeLib.h>
+#include <Console.h>
+#include <stdio.h>
+
+#include <SoftwareSerial.h>
+#include <RH_RF95.h>
+
+#define ONE_WIRE_BUS 2
+#define SENDING_LED 4
+#define MEASURE_LED 3
+#define LORA_TX 5 
+#define LORA_RX 6
+
+#define ROOM_NUMBER 204
+#define NB_MEASURE_PER_SEND 2
+#define DELAY 20000 //1 min 
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST "req"  
+
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+// Singleton instance of the radio driver
+SoftwareSerial ss(5, 6);
+RH_RF95 rf95(ss);
+
+boolean dateSet;
+int room;
+
+void setup(void) {
+ // init temp sensors, led
+  sensors.begin();
+  pinMode(MEASURE_LED, OUTPUT);
+  pinMode(SENDING_LED,OUTPUT);
+  
+
+  Serial.begin(9600);
+  //while (!Serial); // wait for a serial connection
+   if (!rf95.init())
+    {
+        Serial.println("init failed");
+        while(1);
+    }
+  Serial.println("You're Connected");
+  setSyncProvider( requestSync);  //set function to call when sync required
+  Serial.println("Waiting for sync message");
+  rf95.setFrequency(868.0);
+  room = ROOM_NUMBER;
+  dateSet = false;
+  
+  
+  while(!dateSet){
+    if (rf95.available()) {
+      processSyncMessage();
+    }
+    if (timeStatus() == timeSet) {
+     for(int i =0;i<2;i++){ 
+        digitalWrite(MEASURE_LED, HIGH); // LED on if synced
+        digitalWrite(SENDING_LED, HIGH); // LED on if synced
+        delay(500);
+        digitalWrite(MEASURE_LED,LOW);
+        digitalWrite(SENDING_LED,LOW);
+        delay(500);
+     }
+      dateSet = true;
+    } else {
+      //requestSync();
+      digitalWrite(MEASURE_LED, LOW);  // LED off if needs refresh
+    }
+    delay(1000);
+  }
+}
+
+void loop(void) {
+  
+   int nb_measure = 0;
+  String json="[";
+  // Make a HTTP request:
+  while( nb_measure < NB_MEASURE_PER_SEND ){
+    digitalWrite(MEASURE_LED, HIGH);
+    String tempDoc = measureTemp();
+    Serial.println(tempDoc);
+    json += tempDoc+",";
+    Serial.flush();
+    digitalWrite(MEASURE_LED, LOW);
+    if(nb_measure != NB_MEASURE_PER_SEND-1){
+      delay(DELAY); //wait every 30 sec 
+    }
+    
+    nb_measure +=1;
+  }
+  
+  //json = "{\"data\":"+json+"}";
+  uint8_t json_to_send[110];
+  json.toCharArray(json_to_send,110);
+ // Serial.println(json_to_send);
+ 
+ rf95.send(json_to_send,sizeof(json_to_send));
+ rf95.waitPacketSent();
+ digitalWrite(SENDING_LED,HIGH);
+    
+    // Now wait for a reply
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    
+  if(rf95.waitAvailableTimeout(5000))
+    {
+        
+        // Should be a reply message for us now   
+        if(rf95.recv(buf, &len))
+        {
+            Serial.print("got reply: ");
+            Serial.println((char*)buf);
+            digitalWrite(SENDING_LED,LOW);
+        }
+        else
+        {
+            Serial.println("recv failed");
+            radio_error();
+        }
+    }
+    else
+    {
+        Serial.println("No reply, is rf95_server running?");
+        radio_error();
+    }
+    
+    delay(DELAY);
+
+}
+
+void radio_error(){
+   for(int i =0;i<5;i++){ 
+        digitalWrite(SENDING_LED, HIGH); // LED on if synced
+        delay(500);
+        digitalWrite(SENDING_LED,LOW);
+        delay(500);
+     }
+}
+
+String measureTemp(){
+  String tempInfo;
+  sensors.requestTemperatures();
+  char tempVal[5];
+  dtostrf(sensors.getTempCByIndex(0),5,2,tempVal);
+  String tempValString ;
+  for (int i=0;i<5;i++){
+    tempValString += tempVal[i];
+  }
+  time_t t = now();
+  
+  String date= checkDigit(year())+'-'+checkDigit(month())+'-'+checkDigit(day())+'-'+checkDigit(hour())+'-'+checkDigit(minute())+'-'+checkDigit(second());
+    
+  tempInfo +=  "{\"date\":"+date+",\"value\":"+tempValString+",\"room\":"+room+"}";
+  
+  return tempInfo;
+  
+}
+
+
+String checkDigit(int d){
+  String s;
+  if(d < 10)
+    s = "0";
+  return s+String(d);
+  
+}
+
+
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1513581421; // Dec 18 2017
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+    
+  if(rf95.recv(buf, &len)){
+    Serial.println((char*)buf);
+    if((char)buf[0]==TIME_HEADER){
+     String buffer = String((char*)buf).substring(1);
+     Serial.println(buffer);
+     
+     pctime = buffer.toInt();
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Dec 18 2017)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+       Serial.println("setting time");
+     }
+     else{
+      requestSync();
+     }
+    }
+    else{
+      Serial.println("error on time");
+      requestSync();
+    }
+  }
+  else{
+    Serial.println("recv failed");
+  }
+}
+
+time_t requestSync()
+{
+  Serial.println("request sent");
+  uint8_t req[]= TIME_REQUEST;
+  rf95.send(req,sizeof(req)); 
+  rf95.waitPacketSent(); 
+  return 0; // the time will be sent later in response to serial mesg
+}
+
--- Yun/tempSensor/tempSensor.ino
+++ Yun/tempSensor/tempSensor.ino
@@ -1,6 +1,8 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Bridge.h>
+#include <SoftwareSerial.h>
+#include <RH_RF95.h>
 #include <HttpClient.h>
 #include <Process.h>
 #include <Console.h>
@@ -8,61 +10,102 @@
 
 #define ONE_WIRE_BUS 2
 #define SENDING_LED 4
+#define MEASURE_LED 3
 #define ROOM_NUMBER 205
 #define NB_MEASURE_PER_SEND 4
 #define DELAY 30000
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST "req"
 
 Process date;
 Process sendData;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-// Initialize the client library
-HttpClient client;
+
+
+// Singleton instance of the radio driver
+SoftwareSerial ss(10, 11);
+RH_RF95 rf95(ss);
 int room;
 
 void setup(void) {
 
   sensors.begin();
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(MEASURE_LED,OUTPUT);
   pinMode(SENDING_LED,OUTPUT);
-  Serial.begin(115200);
+  Serial.begin(9600);
   Bridge.begin();
-
-  SerialUSB.begin(9600);
-  while (!SerialUSB); // wait for a serial connection
-  SerialUSB.println("You're Connected");
+  if(!rf95.init())
+    {
+        Serial.println("init failed");
+        while(1);
+    } 
+ // while (!SerialUSB); // wait for a serial connection
+  Serial.print("connected");
+  rf95.setFrequency(868.0);
   room = ROOM_NUMBER;
  
   
 }
 
-void loop(void) {
-
-  digitalWrite(SENDING_LED, LOW);
-  
-  int nb_measure = 0;
-  String json="[";
-  // Make a HTTP request:
-  while( nb_measure < NB_MEASURE_PER_SEND ){
-    digitalWrite(LED_BUILTIN, HIGH);
-    String tempDoc = measureTemp();
-    SerialUSB.println(tempDoc);
-    json += tempDoc;
-    SerialUSB.flush();
-    digitalWrite(LED_BUILTIN, LOW);
-    if(nb_measure != NB_MEASURE_PER_SEND-1){
-      json += ",";
-      delay(DELAY); //wait every 30 sec 
+void loop()
+{
+  //Serial.println("waiting");
+  if(rf95.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    //Serial.print(len);
+    if(rf95.recv(buf, &len))
+    {
+        Serial.println((char*)buf);
+        
+        
+        digitalWrite(LED_BUILTIN, HIGH);
+        if(buf[0]==(uint8_t)TIME_REQUEST[0]){ 
+          uint8_t data[15];
+          getTimeNow().toCharArray(data,15);
+          Serial.println("Sending time");
+          rf95.send(data, sizeof(data));
+          rf95.waitPacketSent();
+        }else{          
+          Serial.println("Sending a reply");
+          uint8_t data[]="ok" ;
+          rf95.send(data, sizeof(data));
+          rf95.waitPacketSent();
+          receive(String((char*)buf));
+       }   
+        digitalWrite(LED_BUILTIN, LOW);
     }
-    
-    nb_measure +=1;
+    else
+    {
+        Serial.println("recv failed");
+    }
   }
+}
+
+void receive(String b) {
+  
+  String json= verif(b) ;
+  Serial.println(json);
+  Serial.println(json.length());
+  Serial.println(json.charAt(107));
+  // Make a HTTP request:
+    digitalWrite(MEASURE_LED, HIGH);
+    String tempDoc = measureTemp();
+    Serial.println(tempDoc);
+    json += tempDoc;
+    Serial.flush();
+    digitalWrite(MEASURE_LED, LOW);
+    
   json += "]";
   
   String json_to_send = "{\"data\":"+json+"}";
-  SerialUSB.println(json_to_send);
+  Serial.println(json_to_send);
   
-  if(!sendData.running()){
+  /*if(!sendData.running()){
     digitalWrite(SENDING_LED,HIGH);
     sendData.begin("curl");
     sendData.addParameter("-X");
@@ -72,12 +115,19 @@ void loop(void) {
     sendData.addParameter("Content-Type:application/json");
     sendData.addParameter("--data-binary");
     sendData.addParameter(json_to_send);
-    SerialUSB.println("sent");
+    Serial.println("sent");
     sendData.run();
-  }
+    delay(500);
+    digitalWrite(SENDING_LED, LOW);
+  }*/
  // delay(500);
 
 }
+
+String verif(String s){
+  
+}
+
 
 String measureTemp(){
   String tempInfo,dateVal;
@@ -92,7 +142,7 @@ String measureTemp(){
     if(c!='\n') dateVal += c;
   }
   
-  tempInfo = "{\"date\": \""+dateVal+"\",";
+  tempInfo = "{\"date\":\""+dateVal+"\",";
   sensors.requestTemperatures();
   char tempVal[5];
   dtostrf(sensors.getTempCByIndex(0),5,2,tempVal);
@@ -100,10 +150,32 @@ String measureTemp(){
   for (int i=0;i<5;i++){
     tempValString += tempVal[i];
   }
-  tempInfo +=  "\"value\": "+tempValString+", \"room\": "+room+"}";
+  tempInfo +=  "\"value\":"+tempValString+",\"room\":"+room+"}";
   
   return tempInfo;
   
 }
 
+
+String  getTimeNow(){
+  Serial.println("getting date");
+  String dateVal ;
+  dateVal += TIME_HEADER;
+  // java time is in ms, we want secs    
+  date.runShellCommand("echo `date +%s`");
+  //Serial.println(date);
+ // do nothing until the process finishes, so you get the whole output:
+  while (date.running());
+  //if there's a result from the date process, get it.
+  //Serial.println(date.readString());
+  while (date.available()) {
+    // print the results we got.
+    char c = date.read();
+    if(c!='\n') dateVal += c;
+  }
+  date.runShellCommand("echo `date +%Y-%m-%d-%H-%M-%S`");
+  Serial.println(date.readString());
+  Serial.println(dateVal);
+  return dateVal; 
+}
 
